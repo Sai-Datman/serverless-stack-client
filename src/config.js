37045f0881b6d1f6ae1caa377a3e6fc74d30ad0b
gauravagrawal <gaurav.agrawal@datman.je>
2020-07-10 15:44:57 +0530

    Setting up amplify and resources for app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "note-app-files-upload"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://1nyp63xp1l.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_jijtzpTNf",
+      APP_CLIENT_ID: "4fo8pda3bfjcknfmmn29mb821l",
+      IDENTITY_POOL_ID: "us-east-2:547ac773-340b-44ca-aff9-89c695d04de1"
+    }
+  };
